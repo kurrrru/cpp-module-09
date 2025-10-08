e427c771b46dce17d38914f9baa22083db2fa095
kurrrru <kurrrru8850@gmail.com>
2025-10-08 14:01:21 +0900

    update

--- ex02/detail/1_cmp_neighbor.hpp
+++ ex02/detail/1_cmp_neighbor.hpp
@@ -12,25 +12,67 @@ void cmpNeighbor(const Container<std::pair<T, std::size_t>, std::allocator<std::
         Compare cmp) {
     std::size_t num_elements = container.size();
     std::size_t bigger_size = num_elements / 2;
+    typename Container<std::pair<T, std::size_t>, std::allocator<std::pair<T, std::size_t> > >::const_iterator it = container.begin();
+    for (std::size_t i = 0; i < bigger_size; ++i) {
+        std::pair<T, std::size_t> first = *it;
+        ++it;
+        std::pair<T, std::size_t> second = *it;
+        ++it;
+        if (cmp(first.first, second.first)) {
+            pairs.push_back(std::make_pair(second, first));
+            bigger.push_back(std::make_pair(second.first, i));
+        } else {
+            pairs.push_back(std::make_pair(first, second));
+            bigger.push_back(std::make_pair(first.first, i));
+        }
+    }
+}
 
-    if (is_deque<Container<T, std::allocator<T> > >::value ||
-            is_vector<Container<T, std::allocator<T> > >::value) {
-        if (is_vector<Container<T, std::allocator<T> > >::value) {
-            // pairs.reserve(bigger_size);
-            // bigger.reserve(num_elements);
+// // vectorにテンプレート特殊化する
+template<typename T, typename Compare>
+void cmpNeighbor(const std::vector<std::pair<T, std::size_t> > &container,
+        std::vector<std::pair<std::pair<T, std::size_t>, std::pair<T, std::size_t> > > &pairs,
+        std::vector<std::pair<T, std::size_t> > &bigger,
+        Compare cmp) {
+    std::size_t num_elements = container.size();
+    std::size_t bigger_size = num_elements / 2;
+
+    pairs.reserve(bigger_size);
+    bigger.reserve(bigger_size);
+
+    for (std::size_t i = 0; i < bigger_size; ++i) {
+        std::pair<T, std::size_t> first = container[2 * i];
+        std::pair<T, std::size_t> second = container[2 * i + 1];
+        if (cmp(first.first, second.first)) {
+            pairs.push_back(std::make_pair(second, first));
+            bigger.push_back(std::make_pair(second.first, i));
+        } else {
+            pairs.push_back(std::make_pair(first, second));
+            bigger.push_back(std::make_pair(first.first, i));
         }
-        for (std::size_t i = 0; i < bigger_size; ++i) {
-            std::pair<T, std::size_t> first = container[2 * i];
-            std::pair<T, std::size_t> second = container[2 * i + 1];
-            if (cmp(first.first, second.first)) {
-                pairs.push_back(std::make_pair(second, first));
-                bigger.push_back(std::make_pair(second.first, i));
-            } else {
-                pairs.push_back(std::make_pair(first, second));
-                bigger.push_back(std::make_pair(first.first, i));
-            }
+    }
+}
+
+// // dequeにテンプレート特殊化する
+template<typename T, typename Compare>
+void cmpNeighbor(const std::deque<std::pair<T, std::size_t> > &container,
+        std::deque<std::pair<std::pair<T, std::size_t>, std::pair<T, std::size_t> > > &pairs,
+        std::deque<std::pair<T, std::size_t> > &bigger,
+        Compare cmp) {
+    std::size_t num_elements = container.size();
+    std::size_t bigger_size = num_elements / 2;
+
+    pairs.resize(bigger_size);
+    bigger.resize(bigger_size);
+    for (std::size_t i = 0; i < bigger_size; ++i) {
+        std::pair<T, std::size_t> first = container[2 * i];
+        std::pair<T, std::size_t> second = container[2 * i + 1];
+        if (cmp(first.first, second.first)) {
+            pairs[i] = std::make_pair(second, first);
+            bigger[i] = std::make_pair(second.first, i);
+        } else {
+            pairs[i] = std::make_pair(first, second);
+            bigger[i] = std::make_pair(first.first, i);
         }
-    } else {
-        // neither vector nor deque
     }
 }
--- ex02/detail/2_reorder_pairs.hpp
+++ ex02/detail/2_reorder_pairs.hpp
@@ -6,7 +6,33 @@
 template<typename T, template<typename, typename> class Container>
 void reorderPairs(Container<std::pair<std::pair<T, std::size_t>, std::pair<T, std::size_t> >, std::allocator<std::pair<std::pair<T, std::size_t>, std::pair<T, std::size_t> > > > &pairs,
         const Container<std::pair<T, std::size_t>, std::allocator<std::pair<T, std::size_t> > > &bigger) {
-    Container<std::pair<std::pair<T, std::size_t>, std::pair<T, std::size_t> >, std::allocator<std::pair<std::pair<T, std::size_t>, std::pair<T, std::size_t> > > > orderedPairs(pairs.size());
+    Container<std::pair<std::pair<T, std::size_t>, std::pair<T, std::size_t> >, std::allocator<std::pair<std::pair<T, std::size_t>, std::pair<T, std::size_t> > > > orderedPairs;
+    for (typename Container<std::pair<T, std::size_t>, std::allocator<std::pair<T, std::size_t> > >::const_iterator it = bigger.begin(); it != bigger.end(); ++it) {
+        typename Container<std::pair<std::pair<T, std::size_t>, std::pair<T, std::size_t> >, std::allocator<std::pair<std::pair<T, std::size_t>, std::pair<T, std::size_t> > > >::const_iterator pair_it = pairs.begin();
+        std::advance(pair_it, it->second);
+        orderedPairs.push_back(*pair_it);
+    }
+    pairs.swap(orderedPairs);
+
+}
+
+// vectorにテンプレート特殊化する
+template<typename T>
+void reorderPairs(std::vector<std::pair<std::pair<T, std::size_t>, std::pair<T, std::size_t> > > &pairs,
+        const std::vector<std::pair<T, std::size_t> > &bigger) {
+    std::vector<std::pair<std::pair<T, std::size_t>, std::pair<T, std::size_t> > > orderedPairs;
+    orderedPairs.reserve(pairs.size());
+    for (std::size_t i = 0; i < pairs.size(); ++i) {
+        orderedPairs.push_back(pairs[bigger[i].second]);
+    }
+    pairs.swap(orderedPairs);
+}
+
+// dequeにテンプレート特殊化する
+template<typename T>
+void reorderPairs(std::deque<std::pair<std::pair<T, std::size_t>, std::pair<T, std::size_t> > > &pairs,
+        const std::deque<std::pair<T, std::size_t> > &bigger) {
+    std::deque<std::pair<std::pair<T, std::size_t>, std::pair<T, std::size_t> > > orderedPairs(pairs.size());
     for (std::size_t i = 0; i < pairs.size(); ++i) {
         orderedPairs[i] = pairs[bigger[i].second];
     }
