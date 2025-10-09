96304a4082ceda888af8e04daeb671f9f781059e
kurrrru <kurrrru8850@gmail.com>
2025-10-09 18:34:11 +0900

    update

--- ex02/benchmark/benchmark.cpp
+++ ex02/benchmark/benchmark.cpp
@@ -15,6 +15,7 @@
 #include <ex02/compare/CGreater.hpp>
 #include <ex02/benchmark/sort_functions.hpp>
 #include <ex02/utils/random_seq.hpp>
+#include <toolbox/string.hpp>
 
 namespace {
     void printResult(const std::string &sortName, std::size_t size, std::size_t maxCntCmp, double avgCntCmp, double maxTime, double avgTime) {
@@ -30,30 +31,30 @@ namespace {
 
 void benchmark() {
     const std::size_t numTrials = 10;
-    const std::size_t testSize = 500;
+    const std::size_t testSize = 5000;
     std::vector<std::vector<int> > testVectors(numTrials);
     for (std::size_t i = 0; i < numTrials; ++i) {
         testVectors[i] = generateRandomSequence<int, std::vector>(testSize, 1, 10000);
     }
 
-    std::vector<void (*)(std::vector<int> &, comparer::CLess<int>)> sortFunctions;
-    // sortFunctions.push_back(quickSort<comparer::CLess<int> >);
-    sortFunctions.push_back(mergeSort<comparer::CLess<int> >);
-    // sortFunctions.push_back(heapSort<comparer::CLess<int> >);
-    // sortFunctions.push_back(insertionSort<comparer::CLess<int> >);
-    // sortFunctions.push_back(bubbleSort<comparer::CLess<int> >);
-    // sortFunctions.push_back(selectionSort<comparer::CLess<int> >);
-    // sortFunctions.push_back(binaryInsertionSort<comparer::CLess<int> >);
-    // sortFunctions.push_back(timSort<comparer::CLess<int> >);
-    // sortFunctions.push_back(shellSort<comparer::CLess<int> >);
-    // sortFunctions.push_back(introSort<comparer::CLess<int> >);
-    // sortFunctions.push_back(brickSort<comparer::CLess<int> >);
-    // sortFunctions.push_back(combSort<comparer::CLess<int> >);
-    // sortFunctions.push_back(cycleSort<comparer::CLess<int> >);
-    // sortFunctions.push_back(gnomeSort<comparer::CLess<int> >);
-    // sortFunctions.push_back(patienceSort<comparer::CLess<int> >);
-    // sortFunctions.push_back(smoothSort<comparer::CLess<int> >);
-    // sortFunctions.push_back(tournamentSort<comparer::CLess<int> >);
+    std::vector<std::pair<void (*)(std::vector<int> &, comparer::CLess<int>), std::string> > sortFunctions;
+    // sortFunctions.push_back(std::make_pair(quickSort<comparer::CLess<int> >, "Quick Sort"));
+    sortFunctions.push_back(std::make_pair(mergeSort<comparer::CLess<int> >, "Merge Sort"));
+    // sortFunctions.push_back(std::make_pair(heapSort<comparer::CLess<int> >, "Heap Sort"));
+    // sortFunctions.push_back(std::make_pair(insertionSort<comparer::CLess<int> >, "Insertion Sort"));
+    // sortFunctions.push_back(std::make_pair(bubbleSort<comparer::CLess<int> >, "Bubble Sort"));
+    // sortFunctions.push_back(std::make_pair(selectionSort<comparer::CLess<int> >, "Selection Sort"));
+    // sortFunctions.push_back(std::make_pair(binaryInsertionSort<comparer::CLess<int> >, "Binary Insertion Sort"));
+    // sortFunctions.push_back(std::make_pair(timSort<comparer::CLess<int> >, "Tim Sort"));
+    // sortFunctions.push_back(std::make_pair(shellSort<comparer::CLess<int> >, "Shell Sort"));
+    // sortFunctions.push_back(std::make_pair(introSort<comparer::CLess<int> >, "Intro Sort"));
+    // sortFunctions.push_back(std::make_pair(brickSort<comparer::CLess<int> >, "Brick Sort"));
+    // sortFunctions.push_back(std::make_pair(combSort<comparer::CLess<int> >, "Comb Sort"));
+    // sortFunctions.push_back(std::make_pair(cycleSort<comparer::CLess<int> >, "Cycle Sort"));
+    // sortFunctions.push_back(std::make_pair(gnomeSort<comparer::CLess<int> >, "Gnome Sort"));
+    // sortFunctions.push_back(std::make_pair(patienceSort<comparer::CLess<int> >, "Patience Sort"));
+    // sortFunctions.push_back(std::make_pair(smoothSort<comparer::CLess<int> >, "Smooth Sort"));
+    // sortFunctions.push_back(std::make_pair(tournamentSort<comparer::CLess<int> >, "Tournament Sort"));
 
     // PmergeMe sort function
     std::vector<std::vector<int> > pmergeMeTestVectors = testVectors;
@@ -75,7 +76,9 @@ void benchmark() {
         }
         totalCntCmp += cntCmp;
     }
-    printResult("PmergeMe", testSize, maxCntCmp, static_cast<double>(totalCntCmp) / numTrials, maxTime, static_cast<double>(totalTime) / numTrials);
+    double avgCntCmp = static_cast<double>(totalCntCmp) / numTrials;
+    double avgTime = static_cast<double>(totalTime) / numTrials;
+    printResult("PmergeMe", testSize, maxCntCmp, avgCntCmp, maxTime, avgTime);
 
     // std::sort
     std::vector<std::vector<int> > stdSortTestVectors = testVectors;
@@ -96,7 +99,9 @@ void benchmark() {
         }
         totalCntCmp += cntCmp;
     }
-    printResult("std::sort", testSize, maxCntCmp, static_cast<double>(totalCntCmp) / numTrials, maxTime, static_cast<double>(totalTime) / numTrials);
+    avgCntCmp = static_cast<double>(totalCntCmp) / numTrials;
+    avgTime = static_cast<double>(totalTime) / numTrials;
+    printResult("std::sort", testSize, maxCntCmp, avgCntCmp, maxTime, avgTime);
 
     // std::stable_sort
     std::vector<std::vector<int> > stdStableSortTestVectors = testVectors;
@@ -117,5 +122,33 @@ void benchmark() {
         }
         totalCntCmp += cntCmp;
     }
-    printResult("std::stable_sort", testSize, maxCntCmp, static_cast<double>(totalCntCmp) / numTrials, maxTime, static_cast<double>(totalTime) / numTrials);
+    avgCntCmp = static_cast<double>(totalCntCmp) / numTrials;
+    avgTime = static_cast<double>(totalTime) / numTrials;
+    printResult("std::stable_sort", testSize, maxCntCmp, avgCntCmp, maxTime, avgTime);
+
+    // Other sorting algorithms
+    for (std::size_t funcIdx = 0; funcIdx < sortFunctions.size(); ++funcIdx) {
+        maxTime = 0, totalTime = 0, maxCntCmp = 0, totalCntCmp = 0;
+        for (std::size_t i = 0; i < numTrials; ++i) {
+            std::vector<int> vec = testVectors[i];
+            comparer::CLess<int>::reset();
+            int64 start = get_microseconds();
+            sortFunctions[funcIdx].first(vec, comparer::CLess<int>());
+            int64 end = get_microseconds();
+            int64 elapsed = end - start;
+            std::size_t cntCmp = comparer::CLess<int>::getcnt();
+            if (elapsed > maxTime) {
+                maxTime = elapsed;
+            }
+            totalTime += elapsed;
+            if (cntCmp > maxCntCmp) {
+                maxCntCmp = cntCmp;
+            }
+            totalCntCmp += cntCmp;
+        }
+        std::string sortName = sortFunctions[funcIdx].second;
+        avgCntCmp = static_cast<double>(totalCntCmp) / numTrials;
+        avgTime = static_cast<double>(totalTime) / numTrials;
+        printResult(sortName, testSize, maxCntCmp, avgCntCmp, maxTime, avgTime);
+    }
 }
